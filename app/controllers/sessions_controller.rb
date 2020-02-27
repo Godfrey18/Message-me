83e5fbc4ef10d0ca42018d1da91d8a59adee9e37
Godfrey18 <godfreymenezes07@gmail.com>
2020-02-27 13:24:37 +0530

    authentication completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,28 @@ class SessionsController < ApplicationController
 
 	end
 
+
+def create
+    user = User.find_by(username:params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+    	session[:user_id]= user.id
+    	flash[:success] ="you have been Logged in"
+
+    	redirect_to root_path
+
+    else
+    	flash.now[:error] ="error in loggin"
+    	render 'new'
+	end
+
+end
+
+	def destroy
+    session[:user_id] = nil
+    flash[:success] ="succfully Logged out"
+    redirect_to login_path
+	end
+		
+	
+
 end
